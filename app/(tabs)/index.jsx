f5c9baa3abc6864a53056061f1a247e81ee5e896
Sandip040401 <iemsandip@gmail.com>
2025-01-21 17:47:13 +0530

    updates

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -10,9 +10,9 @@ const app = () => {
       >
         <Text style={styles.title}>Coffee Shop</Text>
 
-        <Link href="/explore" style={{marginHorizontal:'auto'}} asChild>
+        <Link href="/contact" style={{marginHorizontal:'auto'}} asChild>
           <Pressable style={styles.button}>
-            <Text style={styles.buttonText}>Explore</Text>
+            <Text style={styles.buttonText}>Contact</Text>
           </Pressable>
         </Link>
 
